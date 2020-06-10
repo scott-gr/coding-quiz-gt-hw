81e29267acd96feaf0a19f5b6ddbcb8b0e328cf5
Scott Griffin <mscottgriffin@gmail.com>
2020-06-10 10:52:22 -0400

    Condensing question divs and functions into one, will just swap text and buttons via DOM

--- script.js
+++ script.js
@@ -2,79 +2,29 @@
 var startQuiz = document.querySelector("#startbutton");
 // global var for each 'page', so they can be toggled to hide/show in functions
 var homePage = document.querySelector("homepage");
-var qOnePage = document.querySelector("question1");
-var qTwoPage = document.querySelector("question2");
-var qThreePage = document.querySelector("question3");
-var qFourPage = document.querySelector("question4");
-var qFivePage = document.querySelector("question5");
+var quizPage = document.querySelector("question1");
 var scorePage = document.querySelector("scorepage");
 //starting score/timer, may move this into specific function
 var scoreTimer = 60;
 
-//function to change pages. 
-// function nextpage(){
-// //if conditon met - correct answer, advance to next page
-// ///May just incorporate this into each page's function
-
-// }
-
 //function quizbegin
 //if Start Quiz button pressed, 
   //hide homepage
-  //advanced to Question 1
+  //advanced to quizPage
 
-//function firstq
-//show qOnePage
+//function quiz
+//show quizPage
 // If incorrect answer buttons ( a || b || c ...) pressed,
   //scoreTimer -= 15
-  //hide qOnePage
+  //replace question and button text
   //advance to question 2
 //else if correct answer button pressed
-  //hide qOnePage
+  //replace question and button text
   //advance to question 2
-
-//function secondq
-//show qTwoPage
-// If incorrect answer buttons ( a || b || c ...) pressed,
-  //scoreTimer -= 15
-  //hide qTwoPage
-  //advance to question 3
-//else if correct answer button pressed
-  //hide qTwoPage
-  //advance to question 3
-
-//function thirdq
-//show qThreePage
-// If incorrect answer buttons ( a || b || c ...) pressed,
-  //scoreTimer -= 15
-  //hide qThreePage
-  //advance to question 4
-//else if correct answer button pressed
-  //hide qThreePage
-  //advance to question 4
-
-//function fourthq
-//show qFourPage
-// If incorrect answer buttons ( a || b || c ...) pressed,
-  //scoreTimer -= 15
-  //hide qFourPage
-  //advance to question 5
-//else if correct answer button pressed
-  //hide qFourPage
-  //advance to question 5
-
-//function fifthq
-//show qFivePage
-// If incorrect answer buttons ( a || b || c ...) pressed,
-  //scoreTimer -= 15
-  //hide qFivePage
-  //advance to scorePage
-//else if correct answer button pressed
-  //hide qFivePage
-  //advance to scorePage
 
 //function endq
 //show scorePage
+//...
 
 
 
