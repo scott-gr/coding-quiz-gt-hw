bd7061c417d5af887ac5aa9bb6bbe0d4bacb5d61
Scott Griffin <mscottgriffin@gmail.com>
2020-06-12 05:21:13 -0400

    on index.html, moved question buttons into questions container

--- script.js
+++ script.js
@@ -13,68 +13,65 @@ var scorePage = document.querySelector('scorepage');
 var scoreCount = 60;
 
 var arrayQuestions = [
+  //question 1
   {
     question: 'Commonly used data types DO NOT include:',
     buttonA: 'strings',
     buttonB: 'booleans',
     buttonC: 'alerts',
     buttonD: 'numbers',
-    correct: 'C',
+    correct: 'alerts',
   },
+  //question 2
   {
     question: 'The condition in an if/else statement is enclosed within _____.',
     buttonA: 'quotes',
     buttonB: 'curly brackets',
     buttonC: 'parentheses',
     buttonD: 'square brackets',
-    correct: 'C',
+    correct: 'parentheses',
   },
+  //question 3
   {
     question: 'Arrays in JavaScript can be used to store _____.',
     buttonA: 'numbers and strings',
     buttonB: 'other arrays',
     buttonC: 'booleans',
     buttonD: 'all of the above',
-    correct: 'D',
+    correct: 'all of the above',
   },
+  //question 4
   {
-    question: 'String values must be enclosed within _____ when being assigned to variables.',
+    question:
+      'String values must be enclosed within _____ when being assigned to variables.',
     buttonA: 'commas',
     buttonB: 'curly brackets',
     buttonC: 'quotes',
     buttonD: 'parentheses',
-    correct: 'C',
+    correct: 'quotes',
   },
+  //question 5
   {
-    question: 'A very useful tool used during development and debugging for printing content to the debugger is _____.',
+    question:
+      'A very useful tool used during development and debugging for printing content to the debugger is _____.',
     buttonA: 'JavaScript',
     buttonB: 'terminal/bash',
     buttonC: 'for loops',
     buttonD: 'console.log',
-    correct: 'D',
+    correct: 'console.log',
   },
 ];
-// in html id="Question"
 
-// function setQuestion() {
-//   document.getElementById('QuestionSet').innerHTML = '';
-//   var question = document.createElement('question');
-//   question.setAttribute(arrayQuestions[0].question);
+//function checkAnswer
+//if clicked button === correct, text "You are correct!", advance to next question (var # of questions?)
+  //else text "Sorry, that's incorrect", score -= 15, advance to next question
 
 //function quizbegin
 //if Start Quiz button pressed,
 //hide homepage
 //advanced to quizPage
 
-//function quiz
-//show quizPage
-// If incorrect answer buttons ( a || b || c ...) pressed,
-//scoreTimer -= 15
-//replace question and button text
-//advance to question 2
-//else if correct answer button pressed
-//replace question and button text
-//advance to question 2
+
 
 //function endq
 //show scorePage
