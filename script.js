a3be552ea0cedf799bab00e6d397ce99e730cfab
Scott Griffin <mscottgriffin@gmail.com>
2020-06-12 21:28:42 -0400

    wrote pseudo code for functions and event listeners, added variables for buttons on last two pages.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Start Button on homepage
-var startQuiz = document.querySelector('#btnStart');
+var buttonStart = document.querySelector('#btnStart');
 // fills in question text
 var question = document.querySelector('questionText');
 //Buttons for choices A, B, C, D
@@ -7,12 +7,17 @@ var buttonA = document.querySelector('#btnA');
 var buttonB = document.querySelector('#btnB');
 var buttonC = document.querySelector('#btnC');
 var buttonD = document.querySelector('#btnD');
+// button to continue to scoreboard
+var buttonEnd;
+//button to add name to scoreboard
+var buttonName,
 // global var for each 'page', so they can be toggled to hide/show in functions
 var homePage = document.querySelector('homepage');
 var quizPage = document.querySelector('question1');
 var scorePage = document.querySelector('scorepage');
 //starting score/timer, may move this into specific function
 var scoreCount = 60;
+var finalScore ;
 var currentQuestion;
 
 //Array of all questions and answers
@@ -66,17 +71,68 @@ var arrayQuestions = [
   },
 ];
 
-//function checkAnswer
-//if clicked button === correct, text "You are correct!", advance to next question (var # of questions = index in array?)
-  //else text "Sorry, that's incorrect", score -= 15, advance to next question
+//function scoreTimer
+// if scoreCount = 0, end the quiz (gameOver function)
+//show timer element when quiz starts
+//hide timer element qhen quiz ends
+//scoreCount = finalScore at end of quiz
 
-//function quizbegin
-//if Start Quiz button pressed,
-//hide homepage div
-//advanced to quizPage div
+//function askQuestion
+//finalscore = finalscore + scoreCount 
+//if currentQuestion < 5, continue
+///set question and answer text values to current question
+///call checkanswer function
+///else gameOver
 
+//checkanswer function
+//if chosen answer = question
+///display Correct
+///else 
+///display wrong
+/// scorecount -=15
+//currentQuestion ++ 
 
+//gameOver function
+//hide quizpage div
+//show endpage
+//display final score
+//display text Quiz Complete, your score is
+// display submit score button
+//display play again button?
 
-//function endq
-//show scorePage
-//...
+//scoreBoard function
+//hide endpage
+//show scorepage
+// parse local storage and add to scoreboard html
+// prompt enter name
+//click button to add name
+//adds name and score to scoreboard
+//saves to localstorage
+//display play again button
+// 
+
+//Event listeners
+buttonStart.addEventListener('click', function () {
+  //start timer
+  // hide homepage div
+  //show quizpage div
+  //set currentquestion to 1
+});
+//Button A click event listener
+buttonA.addEventListener('click', function () {
+  //checkanswer function for current question, choose answer A
+});
+//Button B click event listener
+buttonB.addEventListener('click', function () {
+  //checkanswer function for current question, choose answer B
+});
+//Button C click event listener
+buttonC.addEventListener('click', function () {
+  //checkanswer function for current question, choose answer C
+});
+//Button D click event listener
+buttonD.addEventListener('click', function () {
+  //checkanswer function for current question, choose answer D
+});
+
+//event listener for adding name to scoreboard
